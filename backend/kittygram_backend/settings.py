76d42e8c33a12fac672283b17b06f2e2ac367e2d
Andrey Martyanov <martyanovav@yandex.ru>
2025-02-22 00:11:42 +0300

    Fix after review

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -10,7 +10,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY', get_random_secret_key())
 
-DEBUG = False
+DEBUG = os.getenv('DEBUG', 'False').lower() == "true"
 
 ALLOWED_HOSTS = ['158.160.74.166', '127.0.0.1', 'localhost', 'kittygramlesson.online']
 
