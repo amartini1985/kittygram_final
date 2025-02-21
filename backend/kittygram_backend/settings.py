8053fe3ef7ee411d8054af61b90c3b7765b20451
Andrey Martyanov <martyanovav@yandex.ru>
2025-02-21 22:04:17 +0300

    Fix bag after review

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -5,18 +5,12 @@ from pathlib import Path
 from django.core.management.utils import get_random_secret_key
 
 BASE_DIR = Path(__file__).resolve().parent.parent
-"""
-SECRET_KEY = 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$'
-"""
-SECRET_KEY = os.getenv('SECRET_KEY', get_random_secret_key())
 
-#DEBUG = False
+SECRET_KEY = os.getenv('SECRET_KEY', get_random_secret_key())
 
 DEBUG = os.getenv('DEBUG', False)
 
-#ALLOWED_HOSTS = ['158.160.74.166', '127.0.0.1', 'localhost', 'kittygramlesson.online']
-
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOST')
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOST', ['127.0.0.1', 'localhost'])
 
 
 INSTALLED_APPS = [
@@ -120,7 +114,6 @@ STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'collected_static'
 
 MEDIA_URL = '/media/'
-#MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 MEDIA_ROOT = '/var/www/kittygram/media/'
 
